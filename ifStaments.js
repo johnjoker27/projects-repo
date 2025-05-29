25a20c2db2e54bdfa63c71b20e5e3253b3bedda7
johnjoker27 <aminuabdulhameed27@gmail.com>
2025-05-28 22:17:00 -0700

    this contains if statements

---
+++ ifStaments.js
@@ -0,0 +1,48 @@
+let age = 25;
+let hasLicense = false;
+
+ if(age >= 16){
+    console.log('You are old enough to drive');
+ }
+
+    if(hasLicense){
+        console.log('you have your license');
+        else{
+            console.log('you do not have your license');
+        }
+    }
+ else{
+    console.log('you are not old enough')
+ }
+let time = 8
+if(time < 12){
+    console.log('good morning')
+}
+else{
+    console.log("good evening")
+}
+ 
+let isStudent = true
+if(isStudent = true){
+    console.log('you are a student')
+}
+else{
+    console.log('you are not a student')
+}
+
+let age = 18;
+
+ if(age >= 100 );{
+    console.log('you are too old to enter the site')
+}
+else if(age >= 18 ){
+    console.log('you are old enough to enter the site');
+}
+else if(age < 0){
+    console.log('your age cannot be below 0');
+}
+
+else{
+    console.log('you must be at least 18 years old to enter');
+}
+    
